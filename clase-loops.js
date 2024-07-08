4d50248eabddbeb095a4fca92a3cd9dfa5a6dd1b
Jassiel Cerda <jassycersot@gmail.com>
2024-07-07 21:10:24 -0700

    Avance extra del dominguito

--- clase-loops.js
+++ clase-loops.js
@@ -32,3 +32,48 @@ for (equipo of semifinalistas) {
     console.log(equipo);
 }
 
+
+
+/*
+//      For In
+
+for in ---> objetos
+
+propiedades = valor
+
+array, string
+
+item
+
+for(variable in objeto) {
+    codigo
+}
+
+*/
+
+const listaCampeonesDelMundo = {
+    Brasil: 5,
+    Italia: 4,
+    Alemania: 4,
+    Argentina: 3,
+    Francia: 2,
+    Uruguay: 2,
+}
+
+for (Nacion in listaCampeonesDelMundo) {
+    console.log(Nacion);
+}
+
+for (Nacion in listaCampeonesDelMundo) {
+    console.log(`${Nacion}: ${listaCampeonesDelMundo[Nacion]}`)
+}
+
+/*
+for (Nacion of listaCampeonesDelMundo) {
+    console.log(Nacion);
+}                           --> Error, For In se utiliza para iterar sobre objetos inumerables, no iterables (arrays, strings)
+
+Para objetos iterables, For Of 
+
+
+*/
